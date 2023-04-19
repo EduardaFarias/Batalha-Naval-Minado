c2e72895cf59b2ccd727f57a4e5339b713b506bc
gangorra <felipegangorra@gmail.com>
2023-04-18 20:34:43 -0400

    Adiciona projeto base

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Projeto-PLP
-Projetos em haskell e prolog da disciplina de PLP
+# BATALHA NAVAL
+---
+
+> #### DO QUE SE TRATA
+
+Esse repositório é dedicado ao projeto da disciplina de Paradigmas de Linguagens de Programação (PLP). Esse projeto séra apresentado em duas linguagens: Haskell (Funcional) e Prolog (Lógica).
+
+<br>
+
+> #### SOBRE O JOGO
+
+Este projeto é um jogo de batalha naval com um adicional de campo minado. Nesse jogo o player tera a opção de escolher jogar com um amigo ou com a máquina, contendo um sistema de rankeada através de um cadastro e contagem de pontos por vitória. O tabuleiro poderá ser definido, o que vai aumentar a dificuldade e o tempo de jogo das batalhas.
+
+[Acesse aqui para saber mais](https://docs.google.com/document/d/1ea-AV-QI9XDrPBfTsM2Qdy2xajnzhF1-gGaIO84EY0Y/edit?usp=sharing)
+
+> #### MEMBROS: 
+
+<br>
+
+###### Maria Eduarda Batista de Farias 
+###### Iasmim Maria Freire da Silva Torres
+###### Felipe da Silva Gangorra
+###### Rayane Bezerra da Silva
+###### Thayane Stheffany Silva Barros
+
+
+
---
+++ batalhaNaval/testeJogo.hs
@@ -0,0 +1,141 @@
+import Control.Concurrent (threadDelay)
+import Control.Exception
+import System.IO
+import System.IO.Error
+import System.Process
+import Data.List
+
+-- definição dos tipos de dados
+type Jogadores = [Jogador]
+type Nome = String
+type Pontuacao = Int
+type Vez = Int
+data Jogador = Jogador Nome Pontuacao
+                    deriving (Show, Read)
+
+-- função que inicia o programa
+main :: IO ()
+main = do   
+          {catch (ler_arquivo) tratar_error;}
+          where
+            ler_arquivo = do
+            {
+              arq <- openFile "dados.txt" ReadMode;
+              dados <- hGetLine arq;
+              hClose arq;
+              inicio_apresentacao (read dados);
+            }
+            tratar_error erro = if isDoesNotExistError erro then do
+            {
+              arq <- openFile "dados.txt" WriteMode;
+              hPutStrLn arq "[]";
+              hClose arq;
+              inicio_apresentacao []
+            }
+            else
+              ioError erro
+
+-- Função que apresenta uma história introduzindo ao jogo e chama o menu
+inicio_apresentacao :: [Jogador] -> IO()
+inicio_apresentacao dados = do
+                  --imprimiIntroducao
+                  putStrLn ("\nPressione <Enter> para continuar")
+                  getChar
+                  menu dados;
+                  return()
+                  
+-- função que irá imprimir a introdução do jogo
+imprimiIntroducao :: IO ()
+imprimiIntroducao = do
+                      system "clear" -- limpa a tela
+                      handle <- openFile "texto.txt" ReadMode
+                      imprimiTextoLentamente handle
+                      hClose handle
+
+imprimiTextoLentamente :: Handle -> IO ()
+imprimiTextoLentamente handle = do
+  texto <- hGetContents handle
+  imprimiTextoLentamenteAux texto
+
+imprimiTextoLentamenteAux :: String -> IO ()
+imprimiTextoLentamenteAux [] = return ()
+imprimiTextoLentamenteAux (x:xs) = do
+  putStr [x]
+  threadDelay 50000 -- intervalo de 50 milissegundos
+  imprimiTextoLentamenteAux xs
+
+
+-- função que exibe o Menu
+menu :: Jogadores -> IO Jogadores
+menu dados = do
+                system "clear" -- limpa a tela
+                putStrLn "-------------------- Batalha Naval --------------------"
+                putStrLn "\n ● Digite 1 para cadastrar jogador"
+                putStrLn "\n ● Digite 2 para jogar"
+                putStrLn "\n ● Digite 0 para sair "
+                putStr "\n→ Opção: \n\n"
+                op <- getChar
+                getChar
+                executarOpcao dados op
+
+
+-- função para manipular a opção escolhida pelo usuário
+executarOpcao :: Jogadores -> Char -> IO Jogadores
+executarOpcao dados '0' = do
+                            putStr("\n A água esquece o nome dos afogados...\n")
+                            return dados
+
+executarOpcao dados '1' = cadastrarJogador dados
+executarOpcao dados '2' = prepararJogo dados
+executarOpcao dados _ = do
+                          putStrLn ("\nOpção inválida! Tente novamente...")
+                          putStrLn ("\nPressione <Enter> para voltar ao menu")
+                          getChar
+                          menu dados
+
+-- função responsável pelo cadastro de jogadores
+cadastrarJogador :: Jogadores -> IO Jogadores
+cadastrarJogador dados = do
+                            system "clear" -- limpa a tela
+                            putStrLn "                  Cadastro de jogadores                  "
+                            putStrLn "\nDigite um nome de usuário: \n"
+                            nome <- getLine
+                            if(existeJogador dados nome) then do
+                              putStrLn "\nEsse nome já existe, escolha outro."
+                              cadastrarJogador dados
+                            else do
+                              arq <- openFile "dados.txt" WriteMode
+                              hPutStrLn arq(show ((Jogador nome 0):dados))
+                              hClose arq
+                              putStrLn ("\nUsuário " ++ nome ++ " cadastrado com sucesso!")
+                              putStrLn ("\nPressione <Enter> para continuar...")
+                              getChar
+                              menu ((Jogador nome 0):dados)
+
+
+getString :: String -> IO String
+getString str = do
+                  putStr str
+                  res <- getLine
+                  return res
+
+existeJogador :: Jogadores -> String -> Bool
+existeJogador dados nome = False
+
+prepararJogo :: Jogadores -> IO Jogadores
+prepararJogo dados = do
+                      system "clear"
+                      putStrLn "-------------------- Batalha Naval --------------------"
+                      putStrLn "\n • Digite 1 para jogar com a máquina"
+                      putStrLn "\n • Digite 2 para jogar com dois jogadores"
+                      putStrLn "\n • Digite 3 para redimensionar o tabuleiro"
+                      putStrLn "\n • Digite 0 para voltar ao menu"
+                      putStr "\n→ Opção: \n\n"
+                      op <- getChar
+                      executarOpcaoJogo dados op
+
+executarOpcaoJogo :: Jogadores -> Char -> IO Jogadores
+executarOpcaoJogo dados '0' = menu dados
+executarOpcaoJogo dados '1' = menu dados 
+executarOpcaoJogo dados '2' = menu dados
+executarOpcaoJogo dados '3' = menu dados
